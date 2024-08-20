0ad98138ea0e7789d1c1d7a5936007ce3324e154
Gerardo Fernandez <gerardo.fernandez@autodesk.com>
2024-08-20 14:10:34 -0700

    commting homework 1 changes 1

--- Week 1/Day 2/aimakerspace/text_utils.py
+++ Week 1/Day 2/aimakerspace/text_utils.py
@@ -1,5 +1,8 @@
 import os
+import fitz  # PyMuPDF
+import re
 from typing import List
+
 
 
 class TextFileLoader:
@@ -11,16 +14,31 @@ class TextFileLoader:
     def load(self):
         if os.path.isdir(self.path):
             self.load_directory()
-        elif os.path.isfile(self.path) and self.path.endswith(".txt"):
-            self.load_file()
+        elif os.path.isfile(self.path):
+            if self.path.endswith(".txt"):
+                self.load_text_file()
+            elif self.path.endswith(".pdf"):
+                self.load_pdf_file()
+            else: 
+                raise ValueError(
+                    "Provided file is not a .txt or .pdf file."
+                )
         else:
             raise ValueError(
-                "Provided path is neither a valid directory nor a .txt file."
+                "Provided path is neither a valid directory nor a .txt or .pdf file."
             )
 
-    def load_file(self):
+    def load_text_file(self):
         with open(self.path, "r", encoding=self.encoding) as f:
             self.documents.append(f.read())
+
+    def load_pdf_file(self):
+        doc = fitz.open(self.path)
+        text = ''
+        for page in doc:
+            text += page.get_text()
+        self.documents.append(text)
+        doc.close()
 
     def load_directory(self):
         for root, _, files in os.walk(self.path):
@@ -30,29 +48,64 @@ class TextFileLoader:
                         os.path.join(root, file), "r", encoding=self.encoding
                     ) as f:
                         self.documents.append(f.read())
+                elif file.endswith(".pdf"):
+                    doc = fitz.open(os.path.join(root, file))
+                    text = ''
+                    for page in doc:
+                        text += page.get_text()
+                    self.documents.append(text)
+                    doc.close()
 
     def load_documents(self):
         self.load()
         return self.documents
 
-
 class CharacterTextSplitter:
-    def __init__(
-        self,
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,
-    ):
-        assert (
-            chunk_size > chunk_overlap
-        ), "Chunk size must be greater than chunk overlap"
-
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
+    def __init__(self, max_chunks: int = 2, max_chunk_size = 1000, overlap_size = 100):
+        self.max_chunks = max_chunks
+        self.max_chunk_size = max_chunk_size
+        self.overlap_size = overlap_size
 
     def split(self, text: str) -> List[str]:
+        pattern = r'(?<=[.!?])\s+(?=[A-Z])'
+        paragraphs = re.split(pattern, text)
         chunks = []
-        for i in range(0, len(text), self.chunk_size - self.chunk_overlap):
-            chunks.append(text[i : i + self.chunk_size])
+        current_chunk = []
+        current_length = 0
+
+        for paragraph in paragraphs:
+
+            if not paragraph.split():
+                continue
+
+            proposed_length = current_length + len(paragraph) + 1
+
+            if current_chunk and proposed_length > self.max_chunk_size:
+                joined_text = '\n'.join(current_chunk)
+                if len(joined_text) > self.overlap_size:
+                    chunks.append(joined_text[:-self.overlap_size])
+                    overlap_text = joined_text[-self.overlap_size:]
+                    current_chunk = [overlap_text + paragraph]
+                    current_length = len(overlap_text) + len(paragraph) + 1
+                else:
+                    current_chunk.append(paragraph)
+                    current_length += len(paragraph) + 1
+            else:
+                current_chunk.append(paragraph)
+                current_length += len(paragraph) + 1
+
+            if len(current_chunk) >= self.max_chunks:
+                joined_text = '\n'.join(current_chunk)
+                chunks.append(joined_text[:-self.overlap_size])
+                overlap_text = joined_text[-self.overlap_size:]
+                current_chunk = [overlap_text]
+                current_length = len(overlap_text)
+
+        if current_chunk:
+            joined_text = '\n'.join(current_chunk)
+            if joined_text.strip():
+                chunks.append(joined_text)
+
         return chunks
 
     def split_texts(self, texts: List[str]) -> List[str]:
