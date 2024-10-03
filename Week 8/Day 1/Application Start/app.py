189b74537023f61cc67be7306ad0fa4b32fa0b51
Gerardo Fernandez <gerardo.fernandez@autodesk.com>
2024-10-03 16:05:53 -0700

    updating the branch

--- Week 8/Day 1/Application Start/app.py
+++ Week 8/Day 1/Application Start/app.py
@@ -1,7 +1,22 @@
 ### Import Section ###
-"""
-IMPORTS HERE
-"""
+import os
+import getpass
+import uuid
+from google.colab import files
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain_qdrant import QdrantVectorStore
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.globals import set_llm_cache
+from langchain_openai import ChatOpenAI
+from langchain_core.caches import InMemoryCache
+from operator import itemgetter
+from langchain_core.runnables.passthrough import RunnablePassthrough
 
 ### Global Section ###
 """
